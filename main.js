fdc7645a4a59d875d5d536f18c14b2aa0b9f886d
yarin <yarin2852003@gmail.com>
2025-02-20 20:38:50 +0200

    update

--- main.js
+++ main.js
@@ -110,22 +110,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 const sectorData = {
     private: {
-        img: "assent/img/WhatsApp Image 2025-02-19 at 21.26.29.jpeg",
+        img: "assent/images/WhatsApp Image 2025-02-19 at 21.26.29.jpeg",
         title: "מגזר פרטי",
         description : "מערכות אבטחה מתקדמות לבית הפרטי, הכוללות מצלמות חכמות מבוססות בינה מלאכותית לזיהוי פנים, ניתוח תנועה בזמן אמת, והתראות מיידיות במקרה של פעילות חשודה. אנו מספקים פתרונות המאפשרים לך לנטר ולשלוט על הנעשה בביתך מכל מקום בעולם באמצעות אפליקציה ייעודית, כולל גיבוי הקלטות ושילוב חיישני דלתות וחלונות למיגון מקיף."
     },
     business: {
-        img: "assent/img/WhatsApp Image 2025-02-19 at 21.16.36.jpeg",
+        img: "assent/images/WhatsApp Image 2025-02-19 at 21.16.36.jpeg",
         title: "מגזר עסקי",
         description: "אנו מציעים פתרונות אבטחה חכמים לעסקים קטנים וגדולים, הכוללים מצלמות אבטחה מתקדמות עם יכולות ניתוח וידאו חכמות לזיהוי עובדים, בקרת כניסה מבוססת זיהוי פנים וטביעת אצבע, והתראות בזמן אמת על ניסיונות חדירה לא מורשים. המערכת שלנו מאפשרת לך לנהל ולנטר את רמת האבטחה בבית העסק בצורה אוטומטית ולמנוע אובדן ציוד או חדירת גורמים בלתי רצויים."
     },
     security: {
-        img: "assent/img/DALL·E 2025-02-19 02.04.06 - A realistic farmland scene with security cameras monitoring the area. A small ATV is trespassing on the private field, and the cameras are actively de.webp",
+        img: "assent/images/DALL·E 2025-02-19 02.04.06 - A realistic farmland scene with security cameras monitoring the area. A small ATV is trespassing on the private field, and the cameras are actively de.webp",
         title: "מגזר ביטחוני",
         description: "מערכות האבטחה שלנו למגזר הביטחוני כוללות פתרונות מתקדמים לשמירה על אתרים רגישים כגון אתרים חקלאיים,אתרי בניה, מתקנים אסטרטגיים ואזורי אבטחה קריטיים. אנו משלבים מצלמות אבטחה באיכות גבוהה עם בינה מלאכותית המסוגלת לזהות התנהגויות חשודות, מערכת בקרת כניסה חכמה לניהול גישה מורשית בלבד, וניתוח נתוני וידאו למניעת איומים פוטנציאליים בזמן אמת."
     },
     corporate: {
-        img: "assent/img/WhatsApp Image 2025-02-19 at 21.16.32.jpeg",
+        img: "assent/images/WhatsApp Image 2025-02-19 at 21.16.32.jpeg",
         title: "מגזר תאגידי",
         description: "פתרונות אבטחה מותאמים במיוחד לתאגידים גדולים ולארגונים בינלאומיים, עם אינטגרציה מלאה למערכות קיימות לניהול אבטחה ארגונית. אנו מספקים מצלמות אבטחה חכמות המאפשרות מעקב בזמן אמת אחר פעילות עובדים, מערכות בקרת כניסה המאפשרות זיהוי עובדים וסינון אורחים באמצעות טכנולוגיית בינה מלאכותית , ופלטפורמות ניתוח נתונים מתקדמות לזיהוי דפוסים חריגים ולמניעת איומים פנים-ארגוניים."
     }
